147950c6601f113a75beb11959b3c34920ebfb5d
Qu1ntV <quint.volwater@hvc.nl>
2023-10-29 15:16:27 +0100

    first commit

---
+++ JS/H1/H1O03.js
@@ -0,0 +1,22 @@
+function setup() {
+  canvas = createCanvas(450,450);
+  background('white');
+  //canvas.parent('processing');
+  noLoop();
+  colorMode(RGB,255,255,255,1);
+}
+
+function draw() {
+  noStroke();
+  fill(2,146,32,0.3);
+ 
+  rect(0,150,450,150);
+
+  rect(0,0,150,450);
+  
+  rect(75,0,375,450);
+
+  ellipse(450,225,450)
+
+  triangle(225,450,225,0,450,225);
+}
